5743833dbc2de657e43367742a41ad6a74c702bb
malvezin <malveblox@gmail.com>
2026-03-18 11:20:19 -0300

    first commit

---
+++ operadores-matematicos.js
@@ -0,0 +1,12 @@
+//comentarios de uma linha apenas
+/* 
+Aqui tem varias linhas de comentarios
+sss
+*/
+//+ adicao/concatenacao, -, *, /, %, resto de divisao, ** potenciacao
+
+const media = 7
+console.log("a media e: "+ media)
+console.log(15*4)
+console.log(100 % 3)
+console.log(2 ** 10)
