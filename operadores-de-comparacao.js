b269140ff28e77cc7217f04de6760315c43cbb59
malvezin <malveblox@gmail.com>
2026-03-19 07:48:19 -0300

    operador-de-comparacao

---
+++ operadores-de-comparacao.js
@@ -0,0 +1,12 @@
+// ==, === Comparam se e igual, !=, !== Comparam se e diferente, ! nao, 
+// >, >==, <, <== maior, menor, maior ou igual, menor ou igual
+
+const umavariavel = "valor"
+console.log(5 == 5)
+console.log(5 == 9)
+console.log(5 == "s")
+console.log(5 == "5")
+console.log(5 === "5")
+console.log(7 != 7)
+console.log(7 != "7")
+console.log(7 !== "7")
